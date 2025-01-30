b693bf42e960b01c7c7ebdf99c77c28a1895d2ea
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2025-01-30 12:38:51 -0300

    feat: adicionando o design inicial da tela

---
+++ painel_V1/build/CMakeFiles/Painel.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Painel.dir/src/main.cpp.o"
+  "CMakeFiles/Painel.dir/src/main.cpp.o.d"
+  "Painel"
+  "Painel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Painel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ painel_V1/painel_V1/build/CMakeFiles/CircularGauge.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CircularGauge.dir/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/CircularGauge.dir/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/CircularGauge.dir/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/CircularGauge.dir/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/CircularGauge.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/CircularGauge.dir/imgui/imgui.cpp.o.d"
+  "CMakeFiles/CircularGauge.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/CircularGauge.dir/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/CircularGauge.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/CircularGauge.dir/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/CircularGauge.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/CircularGauge.dir/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/CircularGauge.dir/src/main.cpp.o"
+  "CMakeFiles/CircularGauge.dir/src/main.cpp.o.d"
+  "CircularGauge"
+  "CircularGauge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CircularGauge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
